e32909c6e94a98c8510d719594d400b344a01d04
shun <3939s.w@gmail.com>
2019-12-02 00:10:32 +0900

    initial commit

---
+++ lib/capifla/version.rb
@@ -0,0 +1,3 @@
+module Capifla
+  VERSION = "0.1.0"
+end
---
+++ spec/capifla_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Capifla do
+  it "has a version number" do
+    expect(Capifla::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
